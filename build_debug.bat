3735b78c97913f9b82fd429cc10f670c21e74fad
michaeljm116 <michaeljm116@gmail.com>
2025-11-01 10:07:06 -0400

    easier zed

--- build_debug.bat
+++ build_debug.bat
@@ -1,17 +1,19 @@
 @echo off
-
 :: This creates a build that is similar to a release build, but it's debuggable.
 :: There is no hot reloading and no separate game library.
 :: add these in teh future -strict-style -vet
 set OUT_DIR=build\debug
-
 if not exist %OUT_DIR% mkdir %OUT_DIR%
-
 odin build src\main_release -debug -subsystem:windows -out:%OUT_DIR%\axiomo_debug.exe
 rcedit-x64.exe %OUT_DIR%\axiomo_debug.exe --set-icon assets/bird.ico
 IF %ERRORLEVEL% NEQ 0 exit /b 1
-
 xcopy /y /e /i assets %OUT_DIR%\assets > nul
 IF %ERRORLEVEL% NEQ 0 exit /b 1
-
 echo Debug build created in %OUT_DIR%
+
+if "%~1"=="run" (
+    echo Running axiomo_debug.exe...
+    pushd %OUT_DIR%
+    start "" axiomo_debug.exe
+    popd
+)
